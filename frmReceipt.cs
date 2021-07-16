47600f36ef89be44da1bae0aa6a548140bc61d36
Mike Be <autofakt5@gmail.com>
2021-07-16 15:56:49 -0700

    got rid of dialog save box and replaced with file writer to bin. Also got file name to be receipt number. Fixed format for the receipt.

--- frmReceipt.cs
+++ frmReceipt.cs
@@ -21,11 +21,48 @@ namespace FinalProjectGUIDraft
             InitializeComponent();
         }
 
+
+        public void saveReceipt()
+        {
+            StreamWriter receiptFile;
+            int tempReceiptNumber = frmMainMenu.receiptCounter-1; //increments earlier so need to offset by 1.
+            receiptFile = File.CreateText(tempReceiptNumber.ToString() + ".txt");
+
+            // Writes receipt information onto a txt file //
+            receiptFile.WriteLine("Receipt Number: " + lblReceiptNumInfo.Text);
+            receiptFile.WriteLine("Customer Name: " + lblCustNameInfo.Text);
+            receiptFile.WriteLine("Date: " + lblDateInfo.Text);
+
+            // For loop iterates through list box of ordered items //
+            for (int i = 0; i < lstItemsOrdered.Items.Count; i++)
+            {
+                // Writes each item ordered onto txt file //
+                receiptFile.WriteLine(lstItemsOrdered.Items[i]);
+            }
+
+            // Writes more receipt information onto a txt file //
+            receiptFile.WriteLine("SubTotal: " + lblSubTotal.Text);
+            receiptFile.WriteLine("Tax: " + lblTaxInfo.Text);
+            receiptFile.WriteLine("Tip: " + lblTipInfo.Text);
+            receiptFile.WriteLine("Total: " + lblGrandTotalInfo.Text);
+            receiptFile.WriteLine("Payment Method: " + lblMethodPymtInfo.Text);
+
+            // Closes file //
+            receiptFile.Close();
+
+            
+
+        }
+
+
+
         // Lee //
         // Executes when user clicks SaveReceipt button //
         // Write receipt information to a file, save it with receipt number, and reset all forms //
         private void btnSaveReceipt_Click(object sender, EventArgs e)
         {
+            saveReceipt();
+            /*
             // Declare a StreamWriter variable //
             StreamWriter receiptFile;
 
@@ -59,22 +96,16 @@ namespace FinalProjectGUIDraft
 
                 // Closes file //
                 receiptFile.Close();
-
-                // Return focus back to Main Menu and clears all controls //
-                frmMainMenu master = (frmMainMenu)Application.OpenForms["frmMainMenu"];
+                */
+            // Return focus back to Main Menu and clears all controls //
+            frmMainMenu master = (frmMainMenu)Application.OpenForms["frmMainMenu"];
                 master.clearMainMenu();
 
                 frmMainMenu.resetFinalArray();
                 // Closes Form
                 this.Close();
 
-            }
-            // Executes else if user selected Cancel from Save As dialog box //
-            else
-            {
-                MessageBox.Show("You clicked Cancel.");
-                saveFileDialog1.Filter = "txt Files (*.txt) |  *.txt";  // Default file type //
-            }
+            
         }
     }
 }
